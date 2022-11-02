7d32a9e25e19b55828b04f76578476ca77c2a5c9
Sai Spandan Reddy <spandanreddy1806@gmail.com>
2022-11-02 19:10:57 -0400

    Second Commit

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 
 const apiConfig = {
-	baseURL:'https://v3.football.api-sports.io/',
-	responseType:'json',
-    headers:{
-    'x-rapidapi-host': 'v3.football.api-sports.io',
-    // // 'x-rapidapi-key': 'XxXxXxXxXxXxXxXxXxXxXxXx',
+    baseURL: 'https://v3.football.api-sports.io/',
+    responseType: 'json',
+    headers: {
+        'x-rapidapi-host': 'v3.football.api-sports.io',
     }
 };
 
 const API = axios.create(apiConfig);
 
-const requests = { api:API ,tis:"myfuck"};
+const requests = { api: API, tis: "myfuck" };
 
 export default requests;
